adcb667f09f23b744cf3ab17782dab26c37a12f3
Flavio Curella <flavio.curella@gmail.com>
2012-07-19 09:05:10 -0500

    removed see, and added a button to show the bug

--- myapp/views.py
+++ myapp/views.py
@@ -1,17 +1,28 @@
 # Create your views here.
 from django.views.generic import TemplateView
-from django_sse.redisqueue import RedisQueueView
 
 from socketio import socketio_manage
 from myapp.namespaces import MyNamespace
+from myapp.utils import redis_connection, emit_to_channel
+from django.views.decorators.csrf import csrf_exempt
 
 
 class HomePage(TemplateView):
     template_name = 'index.html'
 
 
-class SSE(RedisQueueView):
-    pass
+@csrf_exempt
+def incr(request):
+    r = redis_connection()
+    count = r.incr('sheeple')
+    emit_to_channel('default_room', 'myevent', count)
+
+
+@csrf_exempt
+def delete(request):
+    r = redis_connection()
+    r.delete('sheeple')
+    emit_to_channel('default_room', 'myevent', 0)
 
 
 def socketio_service(request):

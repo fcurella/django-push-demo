b9b46a9df0a6d4e1104b4485a8d6d26f1cf398b0
Flavio Curella <flavio.curella@gmail.com>
2013-04-02 16:26:43 -0500

    removed socketio import

--- django_push_demo/urls.py
+++ django_push_demo/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from myapp import views
 
-import socketio.sdjango
-
-socketio.sdjango.autodiscover()
-
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
